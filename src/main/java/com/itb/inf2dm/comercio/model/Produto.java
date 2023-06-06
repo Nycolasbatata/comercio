71f88448ad611f433b71b86de540ff1d04891af6
Nicolas Ozórios <nico1228las@gmail.com>
2023-06-06 09:23:45 -0300

    06/06/23

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -1,5 +1,14 @@
 package com.itb.inf2dm.comercio.model;
 
 public class Produto {
+	private long id;
+	private String descricao;
+	private String nome;
+	private String codigoBarras;
+	private Double preco;
+	
+	//public: modificador de acesso livre para todas as classes
+	//private: Modificador de acesso restrito apenas membros 
+	//da própia classe, Entende-se por "membros" (atributos e/ou metodos)
 
 }
