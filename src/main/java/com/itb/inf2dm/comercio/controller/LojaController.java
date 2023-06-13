476d9e5dd6ff6c59bf0465f2ba9d176017bd5d20
Nicolas Ozórios <nico1228las@gmail.com>
2023-06-13 09:18:07 -0300

    13/06/23

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -1,18 +1,33 @@
 package com.itb.inf2dm.comercio.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itb.inf2dm.comercio.model.Produto;
+
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
 	
 	@GetMapping("/listar")
 	public String listarProdutos() {
+		return "produtos";
 		
-        return "produtos";		
-		 	
+		Produto p1 = new Produto();
+		p1.setId = (20l);
+		p1.setNome("Máquina de lavar Bastemp 12 litros");
+		p1.setCodigoBarras("JHDYEJOUSNEU25411");
+		p1.setPreco(3654.12);
+		
+	listaDeProdutos.add(p1);	 	
+	}
+	@GetMapping("/novo-prod")
+	public String novoProduto() {
+		return "novo-prod";
 	}
 
 }
