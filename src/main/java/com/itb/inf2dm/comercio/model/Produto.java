9baf54733e76650f28d1814e65686780648f45d2
Nicolas Ozórios <nico1228las@gmail.com>
2023-06-13 09:18:55 -0300

    13/06/2023

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -5,10 +5,50 @@ public class Produto {
 	private String descricao;
 	private String nome;
 	private String codigoBarras;
-	private Double preco;
+	private double preco;
 	
 	//public: modificador de acesso livre para todas as classes
 	//private: Modificador de acesso restrito apenas membros 
 	//da própia classe, Entende-se por "membros" (atributos e/ou metodos).
+	
+	//Métodos setter's e Getter's: Adicionar e recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 
+}
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+		
+	}
+	 public double getPreco() {
+		 return preco;
+	 }
 }
